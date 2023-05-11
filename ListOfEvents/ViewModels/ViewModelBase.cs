6002a42db81dfc40a9a0636d76e5f1f7f86699f1
Денис Мурашов <denismur.nsk@gmail.com>
2023-05-12 00:15:00 +0700

    Добавьте файлы проекта.

---
+++ ListOfEvents/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListOfEvents.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ ListOfEvents/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ListOfEvents.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
